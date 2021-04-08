048d5d4a28485f020db667e68a12152faf9580a9
vkutas <kutas.uladzislau@gmail.com>
2021-04-08 03:22:08 +0000

    Break all:(

--- stand_by/setup-standby.sh
+++ stand_by/setup-standby.sh
@@ -2,10 +2,11 @@
 # Clear data dir
 rm -r ${PGDATA}/*
 # Make base copy
-
-echo "${PRIMARY_ADDR}:5432:*:replicator:${REPLICATOR_PASS}" > /var/lib/postgresql/.pgpass
+rm -r "$PGDATA/*"
+export PGPASSFILE=/var/lib/postgresql/.pgpass
+echo "${PRIMARY_ADDR}:5432:replication:replicator:${REPLICATOR_PASS}" > /var/lib/postgresql/.pgpass
 chown postgres:postgres /var/lib/postgresql/.pgpass
-chmod 0600 /var/lib/postgresql/.pgpass
+chmod 600 /var/lib/postgresql/.pgpass
 pg_basebackup -h "$PRIMARY_ADDR" -p 5432 -U replicator -D "${PGDATA}/" -Fp -Xs -R
 
 
@@ -18,6 +19,8 @@ recovery_target_action = 'promote'
 primary_conninfo = '"$PRIMARY_ADDR"  port=5432 user=replicator password="${REPLICATOR_PASS}" application_name=async_standby_node'
     
 FOE
+
+touch  "${PGDATA}/standby.signal"
 
 # Reload configuration
 psql -U postgres -c "SELECT pg_reload_conf();"
